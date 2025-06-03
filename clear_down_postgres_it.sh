59874adc1c27b5836b7260c4c31b137a01bc675c
michael-hmrc <47248104+michael-hmrc@users.noreply.github.com>
2025-06-03 15:47:49 +0100

    update run scripts

---
+++ clear_down_postgres_it.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+docker-compose -f docker-compose.it.db.yml -p dev_quest_test down -v
--- migrations/V1__seed_db_schemas.sql
+++ migrations/V1__seed_db_schemas.sql
@@ -9,14 +9,15 @@ CREATE TABLE users (
     last_name VARCHAR(255),
     user_type VARCHAR(50) CHECK (user_type IN ('Client', 'Dev', 'UnknownUserType')),
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTA
 );
 
 -- Quests table
 CREATE TABLE quests (
     id BIGSERIAL PRIMARY KEY,
-    user_id VARCHAR(255) NOT NULL,
     quest_id VARCHAR(255) NOT NULL,
+    client_id VARCHAR(255) NOT NULL,
+    dev_id VARCHAR(255),
     title VARCHAR(255) NOT NULL,
     description TEXT NOT NULL,
     status VARCHAR(50) NOT NULL DEFAULT 'NotStarted',
